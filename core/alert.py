1faafa205c307f8f845305902d38b2759b1924e2
Christopher Hunt <mail@thisisthechris.co.uk>
2013-04-21 13:09:00 +0100

    Adds Pushover alert for demo

--- core/alert.py
+++ core/alert.py
@@ -1,5 +1,35 @@
 from models import *
+from heatwave.passwords import *
+
+import requests
+
+#For now, use the Pushover API (as Twilio Costs money)
+
+#range = 0.75
 
 #Sends the text alert to Volunteer
-def sendAlert(theVolunteer, theVulnerable):
+def sendAlert(theVulnerable):
+    #print pushoverKey()
+    print theVulnerable.id
     
+    #Find Volunteer
+    
+    theVolunteers = Volunteer.objects.all()
+    for vol in theVolunteers:
+        if abs(vol.geo_lat - theVulnerable.geo_lat) <= 0.3:
+            if not Alert.objects.filter(regarding = theVulnerable):
+	            theAlert = Alert()
+	            theAlert.to = vol
+	            theAlert.regarding = theVulnerable
+	            theAlert.save()
+	            print "Sending.."
+	            the_push_data = {
+	                "token": pushoverKey(),
+	                "user": vol.number,
+	                "message": vol.name + ", Could you go check on " + theVulnerable.name + " at " + theVulnerable.address,
+	                "title": "Alert!",
+	                "url": "http://beta.thisisthechris.co.uk/heatwave/alert/" + str(theAlert.id) + "/",
+	                "url_title": "Complete Check",
+	                "sound": "siren",
+	            }
+	            the_push = requests.post("https://api.pushover.net/1/messages.json", data=the_push_data)
